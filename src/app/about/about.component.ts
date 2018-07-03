7e3628c25212b85a797e3f2572b14f11e8804942
AnilSharma1997 <srkanilsharma@gmail.com>
2018-07-03 11:23:13 +0530

    RxJs Part 1(Observable)

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -19,7 +19,7 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
     this.leaderServices.getLeaders()
-    .then(leaders => this.leaders = leaders);
+    .subscribe(leaders => this.leaders = leaders);
   }
 
 }
